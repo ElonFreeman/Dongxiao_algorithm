a2e9565c54da9a5612a886774738dcb2c2514506
ElonFreeman <zhanghao112358@outlook.com>
2026-04-21 21:20:29 +0800

    26042102somechages

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> arr={9,1,7,6,6,3,2,8};
-
-void quick_sort(int l,int r)
+void quick_sort(int l,int r,vector<int> *arr)
 {
     if(l==r) return;
 
-    int i=l-1,j=r+1,mid=arr[(l+r)/2];
+    int i=l-1,j=r+1,mid=(*arr)[(l+r)/2];
 
     while(i<j)
     {
-        do {i++;} while(arr[i]<mid);
-        do {j--;} while(arr[j]>mid);
-        if(i<j) swap(arr[i],arr[j]);
+        do {i++;} while((*arr)[i]<mid);
+        do {j--;} while((*arr)[j]>mid);
+        if(i<j) swap((*arr)[i],(*arr)[j]);
     }
 
-    quick_sort(l,j);
-    quick_sort(j+1,r);
+    quick_sort(l,j,arr);
+    quick_sort(j+1,r,arr);
 }
 
 int main(void)
 {
+    vector<int> arr={9,1,7,6,6,3,2,8};
+
+    quick_sort(0,size(arr)-1,&arr);
     
-    quick_sort(0,size(arr)-1);
     for(int output:arr)
     {
         cout << output <<' ';
